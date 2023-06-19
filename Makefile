5709e92fffaa95ff654514d4416413c578655405
AlexisHamon <alexis.hamon24@gmail.com>
2023-06-19 10:40:47 +0200

    add some test scripts

--- Makefile
+++ Makefile
@@ -1,9 +1,22 @@
+all: time_benchmark unit_test all_unit_tests
 
+unit_test: unit_test.sh
+	echo "#! " /bin/sh > $@
+	cat $< >> $@
+	chmod ugo+x $@
 
 time_benchmark: utilities/time_benchmark.sh
-	echo "#! " /bin/sh > time_benchmark
-	cat utilities/time_benchmark.sh >> time_benchmark
-	chmod ugo+x time_benchmark
+	echo "#! " /bin/sh > $@
+	cat $< >> $@
+	chmod ugo+x $@
+
+all_unit_tests: all_unit_tests.sh
+	echo "#! " /bin/sh > $@
+	cat $< >> $@
+	chmod ugo+x $@
+	./test
+
 
 clean:
-	rm time_benchmark
+	rm -f time_benchmark unit_test all_unit_tests
+	rm tmp/*
---
+++ unit_test.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+PLUTO_PATH=$(realpath "../pluto")
+POLYBENCH_PATH=$PWD
+export PATH="$PLUTO_PATH:$PATH"
+
+file=$1
+dirname=$(dirname "$file")
+r_basename=$(basename "$file")
+basename=$(basename "$file" .c)
+
+dir="tmp/"
+
+cp "$file" "$dir"bench.c 
+
+exe_ref="$dir"bench_ref
+exe_pluto="$dir"bench_pluto
+
+(cd "$dir" &&  polycc --silent --second-level-tile --tile --nounrolljam "$POLYBENCH_PATH/$file" -o bench_pluto.c)
+
+
+gcc -O3 -lm -I utilities -I "$dirname" utilities/polybench.c "$dir"bench_pluto.c -DPOLYBENCH_DUMP_ARRAYS -DDIV0=8 -DDIV1=32 -DDIV2=64 -o "$exe_pluto"
+gcc -O0 -I utilities -I "$dirname" utilities/polybench.c "$dir"bench.c -DPOLYBENCH_DUMP_ARRAYS -o "$exe_ref"
+
+cat /dev/null > "$exe_ref".out
+cat /dev/null > "$exe_pluto".out
+$exe_ref 2> "$exe_ref".out
+$exe_pluto 2> "$exe_pluto".out
+
+
+if ! cmp "$exe_ref".out "$exe_pluto".out; then
+    printf "\e[31mFailed\e[0m %s" "$file"!
+    exit 0
+else
+    printf "\e[32mPassed\e[0m"
+    exit 1
+fi
