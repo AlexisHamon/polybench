9ba96abc9640d61bc7ca3fc7b9b2be1ac3147165
Pernias <mpernias@unistra.fr>
2023-06-27 17:12:47 +0200

    ajout get_best donne meilleur config

---
+++ get_best.py
@@ -0,0 +1,46 @@
+import csv
+import glob
+import sys
+def get_best_execution_time(data,exec_type0):
+    best_time = float('inf')
+    best_div1, best_div2, best_div3, best_exec_type, best_extra = None, None, None, None, None
+
+    for row in data:
+        div1 = int(row['DIV1'])
+        div2 = int(row['DIV2'])
+        div3 = int(row['DIV3'])
+        flags = row['CFLAGS']
+        exec_type = row['EXEC_TYPE']
+        extra = row['EXTRA_FLAGS']
+        time = float(row['TIME'])
+        if time < best_time and str(exec_type0) == str(exec_type):
+            best_time = time
+            best_div1, best_div2, best_div3, flags, best_extra = div1, div2, div3, flags, extra
+
+    return best_div1, best_div2, best_div3, flags, best_extra
+#Le nom du fichier est le premier argument
+filename = sys.argv[1]
+#Type d'execution
+exec_type = sys.argv[2]
+
+#On cherche dans tous les fichiers csv qui commence par le nom du fichier
+files=glob.glob("../mark5/processed_data/"+filename+"*.csv")
+for file in files:
+    with open(file, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        data = list(csv_reader)
+
+    # Récupérer les meilleurs DIV1, DIV2, DIV3 et FLAGS
+    best_div1, best_div2, best_div3, best_flag, best_extra = get_best_execution_time(data,exec_type)
+
+    # Afficher les résultats
+    print(best_div1)
+    print(best_div2)
+    print(best_div3)
+    print(best_flag)
+    print(best_extra)
+
+
+
+
+    
